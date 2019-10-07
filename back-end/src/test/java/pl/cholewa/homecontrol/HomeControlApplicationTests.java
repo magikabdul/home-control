bf056f97992060b5fe0e63a93dbc4e0c53781f98
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-10-07 09:12:23 +0200

    chore: initial commit for back-end

---
+++ back-end/src/test/java/pl/cholewa/homecontrol/HomeControlApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.cholewa.homecontrol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomeControlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
